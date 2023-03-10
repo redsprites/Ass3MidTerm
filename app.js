3fad196842ca5e088f936d57190719437be13cdf
redsprites <71545773+redsprites@users.noreply.github.com>
2023-03-10 17:29:05 -0500

    user changed

--- app.js
+++ app.js
@@ -3,22 +3,17 @@ const blogs={
 	index: function () {
 		$(document).ready(function() {
 			var btns = $("#btns");
-			var index = getAllUrlParams().page;
-			
+			var index = getAllUrlParams().page;	
 			if (index == null) {
 			  index = 0;
 			}
-			
 			database.index(blogs.documentID, function (items){
 			  if (index >= (items.length / 4) || index < 0 || index == null) {
 				$("body").html(`<h1 style="color: #555; font-size: 50px; margin-top: 50px; text-align: center; font-family: Arial, sans-serif">Error 404: Page not found</h1><p style = "font-size: 20px; color: #888; margin-bottom: 50px; text-align: center; font-family: Arial, sans-serif">Sorry, the page you're looking for doesn't exist.</p>`);
-			  }
-			  
+			  }	  
 			  btns.append(`<a href="index.html?page=${(index - 1) < 0 ? ((items.length / 4) - 1) : (index - 1)}" role="button" class="btn btn-outline-secondary">Previous</a>
 			  <a href="index.html?page=${(parseInt(index) + 1) >= (items.length / 4) ? 0 : (parseInt(index) + 1)}" role="button" class="btn btn-outline-secondary">Next</a>`);
-			});
-		  
-		  
+			});  
 		$('#blogs').html('Loading Blogs, please wait...');
 		database.index(blogs.documentID, function (items) {
 		  $('#blogs').empty();
@@ -28,8 +23,6 @@ const blogs={
 			}
 		  for (let i = index*4; i < endIndex; i++) {
 			let blog = items[i];
-			console.log(i);
-			console.log(blog.title);
 			let el = $('<div>').html(`
 						<div class="post-preview">
 							<a href="post.html?index=${i}">
@@ -37,7 +30,7 @@ const blogs={
 								<h3 class="post-subtitle">${blog.subTitle}</h3>	
 							</a>	
 							<p class="post-meta">
-							Posted by <a href="#!">${blog.author}</a>
+							Posted by <a href="user.html?index=${i}">${blog.firstName} ${blog.lastName}</a>
 							on ${blog.blogDate}
 							</p>	
 						</div>
@@ -56,7 +49,8 @@ const blogs={
 		  $('#loading').hide();
 		  $('#post-title').text(item.title);
 		  $('#post-sub-title').text(item.subTitle);
-		  $('#blog-author').text(item.author);
+		  $('#blog-first-name').text(item.firstName);
+		  $('#blog-last-name').text(item.lastName);
 		  $('#blog-text').text(item.blog);
 		  $('#blog-date').text(item.blogDate);
 		  $('#btn-edit').attr('href', `edit.html?index=${index}`);
@@ -90,20 +84,43 @@ const blogs={
 		  }
 		});
 	  },
+	  displayUser: function (index) {
+		database.detail(blogs.documentID, index, function (item) {
+			$('#loading').hide();
+			let el = $('<div>').html(`
+			<div class="user-preview">
+					<p class="first-name">First Name: ${item.firstName}</p>
+					<p class="last-name">Last Name: ${item.lastName}</p>
+					<p class="user-name">User Name: ${item.lastName}</p>
+					<p class="email">Email: ${item.email}</p>
+					<p class="internship">Looking for Internship: ${item.internship}</p>		
+			</div>
+		`);	
+		$('#user-preview').append(el);
+		});
+	  },
 	  create: function () {
 		$('form').on('submit', function (e) {
 		  e.preventDefault();
-		  let author = $('form input[name=author]');
+		  let firstName = $('form input[name=firstName]');
+		  let lastName = $('form input[name=lastName]');
 		  let title = $('form input[name=title]');
 		  let subTitle = $('form input[name=subTitle]');
+		  let userName = $('form input[name=userName]');
+		  let email = $('form input[name=email]');
+		  let internship = $('form input[name=internship]');
 		  let blog = $('form textarea[name=blog]');
 		  const date = new Date();
 		  const todaysDate = date.toLocaleDateString();
 	
 		  let newBlog = {
-			author: author.val(),
+			firstName: firstName.val(),
+			lastName: lastName.val(),
 			title: title.val(),
 			subTitle: subTitle.val(),
+			userName: userName.val(),
+		  	email: email.val(),
+		  	internship: internship.val(),
 			blog: blog.val(),
 			blogDate: todaysDate,
 		  };
@@ -113,23 +130,32 @@ const blogs={
 	  update:function(index){
 		database.detail(blogs.documentID,index,function(item){
 			$('#loading').hide();
-			$('input[name=author]').val(item.author);
+			$('input[name=firstName]').val(item.firstName);
+			$('input[name=lastName]').val(item.lastName);
 			$('input[name=title]').val(item.title);
 			$('input[name=subTitle]').val(item.subTitle);
 			$('textarea[name=blog]').val(item.blog);
 
 			$('form').submit(function(e){
 				e.preventDefault();
-				let author=$('input[name=author]').val();
+				let firstName=$('input[name=firstName]').val();
+				let lastName=$('input[name=lastName]').val();
 				let title=$('input[name=title]').val();
 				let subTitle = $('input[name=subTitle]').val();
+				let userName = $('form input[name=userName]').val();
+		 		let email = $('form input[name=email]').val();
+				let internship = $('form input[name=internship]:checked').val();
 				let blog=$('textarea[name=blog]').val();
 				const date = new Date()
 				const todaysDate = date.toLocaleDateString();
 				let newBlog={
-					author:author,
+					firstName:firstName,
+					lastName:lastName,
 					title: title,
 					subTitle:subTitle,
+					userName: userName,
+		  			email: email,
+		  			internship: internship,
 					blog:blog,
 					blogDate: todaysDate
 				};
